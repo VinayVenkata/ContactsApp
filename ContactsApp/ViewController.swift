af8020ddb1f71e931a19ecc59096e79adfa508fb
vinayPonnuri <vinayponnuri@gmail.com>
2018-08-06 02:05:15 -0400

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Vinay Ponnuri on 8/6/18.
+//  Copyright © 2018 Vinay Ponnuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
